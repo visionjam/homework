e92b81fe964fb9e8b8ad52ce4d687f57953c5684
Vison Jam <visionjam777@gmail.com>
2026-03-13 16:41:20 +0800

    Delete 2026.3.5 directory

--- 2026.3.5/2026_3_5_3.py
+++
@@ -1,9 +0,0 @@
-import math
-angle=float(input("请输入角度:"))
-x=float(input("请输入浮点数值x："))
-radian=math.pi/180*angle
-sin1=math.sin(radian)
-log1=math.log((math.fabs(x))+1)
-sqrt1=math.sqrt(x*x+1)
-a=sin1+log1-sqrt1
-print("表达式结果:",a)
