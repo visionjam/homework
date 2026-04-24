da2b1dd1f228ff8fd0a791e8ae5be864fd6c3cab
visionjam <visionjam777@gmail.com>
2026-04-24 13:22:11 +0800

    机房作业补交第6题

---
+++ python/2026.4.18/2026.4.23.6.py
@@ -0,0 +1,10 @@
+import random
+random.seed(1000)
+matrix=[[random.randint(10,30) for i in range(0,5)] for o in range(0,10)]
+print("原始矩阵：")
+for i in matrix:
+    print(i,end="\n")
+trans=[[raw[i] for raw in matrix] for i in range(len(matrix[0]))]
+print("转置矩阵：")
+for i in trans:
+    print(i,end="\n")
